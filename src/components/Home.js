f79e7f1b5ad4ff6c74a0d0315f738f2fe5037a5c
saurabh915 <saurabhspatil4545@gmail.com>
2022-06-26 19:57:17 +0530

    deploying on heroku

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ import Notes from './Notes'
 
 
 export const Home = (props) => {
- const {showAlert} = props
+ const {showAlert} = props//when we have to pass same prop to another componenent then we use destructuring and pass the same component to another component
 
   return (
     <div className = "container my-3">
